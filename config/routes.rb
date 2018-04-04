c869af194e49dd51ebce07f0160771d67d6eb024
Justin A. Parado <japarado@student.apc.edu.ph>
2018-04-04 15:57:21 +0800

    loogin/signup views for hr and faculty user types

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :faculties
+  devise_for :hrs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
