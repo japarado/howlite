a12e35548653920533cba335a8299a22afecf6f3
Justin Parado <japarado@student.apc.edu.ph>
2018-04-07 13:50:49 +0800

    hr scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  resources :hrs
   devise_for :users
-  devise_for :hrs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
