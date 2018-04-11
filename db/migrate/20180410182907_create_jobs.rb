5b0d7a8fc0ba5a201150a7eace5f53a07041aabd
Justin A. Parado <japarado@student.apc.edu.ph>
2018-04-11 22:47:58 +0800

    added hr_id fk in job migration

--- db/migrate/20180410182907_create_jobs.rb
+++ db/migrate/20180410182907_create_jobs.rb
@@ -1,6 +1,7 @@
 class CreateJobs < ActiveRecord::Migration[5.1]
   def change
     create_table :jobs do |t|
+      t.belongs_to :hr
       t.string :title
       t.string :job_type
       t.decimal :salary
