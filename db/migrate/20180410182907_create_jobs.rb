b4f479e18970272e90cee8b3e58e8d0fd7091676
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-04-11 15:04:55 +0800

    job migrations

--- db/migrate/20180410182907_create_jobs.rb
+++ db/migrate/20180410182907_create_jobs.rb
@@ -1,7 +1,10 @@
 class CreateJobs < ActiveRecord::Migration[5.1]
   def change
     create_table :jobs do |t|
-
+      t.string :title
+      t.string :type
+      t.decimal :salary
+      t.text :desc
       t.timestamps
     end
   end
