ee4154db4da43736ae57a3d0e405c0cc416956a2
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-04-13 17:15:47 +0800

    authenticates user, adjusted registration, login system/register done
    Co-Authored-By: Justin Parado <japarado@users.noreply.github.com>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//=
 //= require ./js/jquery-min
 //= require ./js/bootstrap.min
 //= require ./js/material.min
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   #root
   root 'pages#home', as: 'home'
 
+  #users
+  devise_for :users,:controllers=>{registrations:'registrations'}
+
   resources :profiles
   resources :faculties
   resources :hrs
@@ -12,6 +15,6 @@ Rails.application.routes.draw do
   #jobs
   resources :jobs
 
-  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
