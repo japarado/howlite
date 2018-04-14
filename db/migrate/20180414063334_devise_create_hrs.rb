fcd67f02ca2996d2900737138709a8a23fd75102
Justin Parado <japarado@student.apc.edu.ph>
2018-04-14 14:37:37 +0800

    nullable names in hr and user

--- db/migrate/20180414063334_devise_create_hrs.rb
+++ db/migrate/20180414063334_devise_create_hrs.rb
@@ -8,7 +8,7 @@ class DeviseCreateHrs < ActiveRecord::Migration[5.1]
       t.string :encrypted_password, null: false, default: ""
 
       #Custom Fields
-      t.string :name
+      t.string :name, null: false
 
       ## Recoverable
       t.string   :reset_password_token
