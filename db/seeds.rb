167d6f671b136867712faf6ee48348e741f5dee2
Justin Parado <japarado@student.apc.edu.ph>
2018-04-14 16:22:02 +0800

    seeds and controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,47 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# create a constant FACULTY
+@faculty_user = User.new
+@faculty_user.email = 'pamity@mail.com'
+@faculty_user.name = 'Pamity'
+@faculty_user.password = 'secret'
+@faculty_user.user_type = 'FACULTY'
+@faculty_user.save
+Faculty.create(:user_id => 1)
+
+#create a constant HR
+@hr_user = User.new
+@hr_user.email = 'apc@mail.com'
+@hr_user.name = 'Asia Pacific College'
+@hr_user.password = 'secret'
+@hr_user.user_type = 'HR'
+@hr_user.save
+Hr.create(:user_id => 2)
+
 # create 10 users FACULTY
 10.times do
   User.create(
       :email => Faker::Internet.email,
-      :name => Faker::name,
+      :name => Faker::Name.name,
       :password => 'secret',
       :user_type => 'FACULTY'
   )
 end
 
-=begin
-User.all.each do |user|
-  Faculty.create(:user_id => user.id)
-end
-=end
-
 10.times do
   User.create(
       :email => Faker::Internet.email,
-      :name => Faker::name,
+      :name => Faker::Name.name,
       :password => 'secret',
       :user_type => 'HR'
   )
+end
+
+User.all.each do |user|
+  if user.user_type 'FACULTY'
+    Faculty.create(:user_id => user.id)
+  else
+    Hr.create(:user_id => user.id)
+  end
 end
