967ece8be89fef6ea3b9c5a5288119b44c657fe4
Justin A. Parado <japarado@student.apc.edu.ph>
2018-04-19 07:19:39 +0800

    form validation for job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,8 @@
 class Job < ApplicationRecord
+  validates :title, presence: true
+  validates :job_type, presence: true
+  validates :salary, presence: true
+  validates :desc, presence: true
 
   belongs_to :hr
 
