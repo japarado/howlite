bc660ca638404c1d7465eb426a5ead386a67907c
Justin A. Parado <japarado@student.apc.edu.ph>
2018-04-18 17:25:57 +0800

    quick jobs logic fix

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -27,7 +27,7 @@ class JobsController < ApplicationController
   # POST /jobs.json
   def create
     @job = Job.new(job_params)
-
+    @job.hr_id = current_user.id
     respond_to do |format|
       if @job.save
         format.html { redirect_to @job, notice: 'Job was successfully created.' }
