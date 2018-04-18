a2bef5f94194b764044094731403b0994d3afe09
Justin A. Parado <japarado@student.apc.edu.ph>
2018-04-18 15:36:12 +0800

    applications scaffold and relationship

--- app/models/hr.rb
+++ app/models/hr.rb
@@ -1,4 +1,7 @@
 class Hr < ApplicationRecord
   belongs_to :user
   has_many :jobs
+
+  has_many :applications
+  has_many :jobs, through: :applications
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,9 @@ class Job < ApplicationRecord
 
   belongs_to :hr
 
+  has_many :applications
+  has_many :jobs, through: :applications
+
   validates :title, presence:true
   validates :job_type, presence:true
   validates :salary, presence:true
