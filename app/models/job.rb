eef7efc48b5b9d64ec05742e76663071b0b03d82
Justin A. Parado <japarado@student.apc.edu.ph>
2018-04-21 08:40:01 +0800

    finished job scope search

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,18 +1,25 @@
 class Job < ApplicationRecord
+  #enum type: [:pt,:ft,:greater_than_20k,:greater_than_30k]
+
   validates :title, presence: true
   validates :job_type, presence: true
   validates :salary, presence: true
   validates :desc, presence: true
 
   belongs_to :hr
-
   has_many :applications
   has_many :faculties, :through => :applications
 
-  validates :title, presence: true
-  validates :job_type, presence: true
-  validates :salary, presence: true
-  validates :desc, presence: true
+  scope :job_type, -> (job_type) {where job_type: job_type}
+  scope :term, -> (search_term) {where("title like ? or desc like ?", "%#{search_term}%", "%#{search_term}%")}
+  scope :salary, -> (expected_salary) {where("salary > ?", "#{expected_salary}")}
 
-  scope :part_time, -> {where(job_type: "PT")}
+  #scope :part_time, -> {where(job_type: "PT")}
+  #scope :full_time, -> {where(job_type: "FT")}
+  #scope :average, -> {  }
+
+  def self.send_chain(methods)
+    methods.inject(self, :send)
+  end
+
 end
