a5c61c1c25c9c51a04aa334a05861cc687931f14
Joshua Morales <joshuao.moralesjm@gmail.com>
2018-04-21 10:35:19 +0800

    Add attachment column to documents
    oh yis

--- db/migrate/20180418070649_create_documents.rb
+++ db/migrate/20180418070649_create_documents.rb
@@ -3,6 +3,7 @@ class CreateDocuments < ActiveRecord::Migration[5.1]
     create_table :documents do |t|
       t.belongs_to :document_space, index: true
       t.string :name
+      t.string :attachment
       t.timestamps
     end
   end
