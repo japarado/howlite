14a3069a050871334b1b3a69c0fb312fda12bef2
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-04-21 10:39:43 +0800

    Revert "Merge branch 'master' of https://github.com/japarado/howlite"
    This reverts commit 7a1589b8c74d9f7a0ead35f02e1977ef0d789d49, reversing
    changes made to 15d00b238810e8be69fbb1af736c3f4a07a67703.

--- db/migrate/20180418070649_create_documents.rb
+++ db/migrate/20180418070649_create_documents.rb
@@ -2,7 +2,7 @@ class CreateDocuments < ActiveRecord::Migration[5.1]
   def change
     create_table :documents do |t|
       t.belongs_to :document_space, index: true
-      t.string :attachment
+      t.string :name
       t.timestamps
     end
   end
